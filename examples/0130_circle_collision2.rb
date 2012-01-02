5bac12bae4df8dc1b88dde64501efe8696857fd1
akicho8 <akicho8@gmail.com>
2012-01-03 03:04:06 +0900

    円同士の当り判定デモ(ベクトル版)の追加

--- examples/0130_circle_collision2.rb
+++ examples/0130_circle_collision2.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# 円同士の当り判定(従来の方法)
+# 円同士の当り判定(ベクトルを使った方法)
 #
 require File.expand_path(File.join(File.dirname(__FILE__), "helper"))
 
@@ -12,25 +12,23 @@ class Ball
   end
 
   def update
-    distance = @pos.distance(@base.cursor)
     radius2 = @radius + @base.cursor_radius
-    if distance < radius2
-      @base.draw_line2(@pos, @base.cursor)
+    diff = @pos - @base.cursor
+    if diff.length < radius2
+      # @base.draw_line2(@pos, @base.cursor)
       @base.vputs "COLLISION"
 
       if @base.button.btA.press?
         # カーソルの方向から円の位置の方向に一方的に移動させる
-        dir = @base.cursor.angle_to(@pos)
-        @pos.x = @base.cursor.x + Stylet::Fee.cos(dir) * radius2
-        @pos.y = @base.cursor.y + Stylet::Fee.sin(dir) * radius2
+        @pos = @base.cursor + diff.normalize.scale(radius2)
+        # @pos += diff.scale(diff.length)
       end
     end
     @base.draw_circle(@pos, :radius => @radius, :vertex => 32)
-    if @base.count.modulo(5) == 0
-      @base.draw_line2(@pos, @base.cursor)
-    end
-    @base.vputs "radius2=#{radius2}"
-    @base.vputs "distance=#{distance}"
+    # if @base.count.modulo(5) == 0
+    #   @base.draw_line2(@pos, @base.cursor)
+    # end
+    # @base.vputs "radius2=#{radius2}"
   end
 
   def screen_out?
