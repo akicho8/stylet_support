654aa9a9885a4f307c2b3e72321085d616783256
akicho8 <akicho8@gmail.com>
2012-01-13 12:14:04 +0900

    ファイル名のリナンバー

---
+++ examples/0270_collision_circle_vs_dot.rb
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+# 円と点の衝突と反発
+#
+require File.expand_path(File.join(File.dirname(__FILE__), "helper"))
+
+class Circle
+  def initialize(win)
+    @win = win
+
+    @radius = 64                                     # 円の半径
+
+    @pos = Stylet::Vector.new(@win.rect.center.x, @win.rect.max_y)             # 物体初期位置
+    @speed = Stylet::Vector.new(Stylet::Etc.wide_rand(2.0), Stylet::Etc.range_rand(-12, -15)) # 速度ベクトル
+    @gravity = Stylet::Vector.new(0, 0.220)                                                        # 重力
+  end
+
+  def update
+    @speed += @gravity
+    @pos += @speed
+
+    diff = @pos - @win.cursor
+    if diff.length > 0
+      if diff.length < @radius
+        @pos = @win.cursor + diff.normalize.scale(@radius)
+        @speed = diff.normalize * @speed.length
+      end
+    end
+
+    @win.draw_circle(@pos, :radius => @radius, :vertex => 32)
+    @win.vputs "Z:x++ X:x--"
+  end
+end
+
+class App < Stylet::Base
+  include Helper::TriangleCursor
+
+  def before_main_loop
+    super if defined? super
+    @objects << Circle.new(self)
+    @cursor_radius = 1
+  end
+end
+
+App.main_loop
